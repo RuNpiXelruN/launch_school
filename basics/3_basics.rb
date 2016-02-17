73773ef8304dc7d20c1b33e594136dadba9b6d7f
runpixelrun <justindavidson23@gmail.com>
2016-02-17 04:57:27 +0000

    Completed basics section exercises

---
+++ basics/3_basics.rb
@@ -0,0 +1,10 @@
+movies = { indiana_jones: 1990,
+           bourne_identity: 2004,
+           jurassic_park: 1996,
+           jumanji: 2001
+         }
+
+puts movies[:indiana_jones]
+puts movies[:bourne_identity]
+puts movies[:jurassic_park]
+puts movies[:jumanji]
---
+++ basics/4_basics.rb
@@ -0,0 +1,6 @@
+year = [1990, 2004, 1996, 2001]
+
+puts year[0]
+puts year[1]
+puts year[2]
+puts year[3]

d704f432fa853166a294f6fcae596a8b6df42753
runpixelrun <justindavidson23@gmail.com>
2016-02-17 08:36:49 +0000

    Completed exercises in 'loops' section

---
+++ loops/2_loops.rb
@@ -0,0 +1,10 @@
+
+x = ""
+
+while x != "stop"
+    puts "how was your day?"
+    ans = gets.chomp
+    
+    puts "Ask you again?"
+    x = gets.chomp
+end
---
+++ loops/3_loops.rb
@@ -0,0 +1,10 @@
+
+arr = [1,23,456,3,2436]
+
+arr.each_with_index do |x, index|
+    puts "--------"
+    puts "#{index}: #{x}"
+end
+
+
+    

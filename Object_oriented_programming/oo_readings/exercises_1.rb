c05772986763d6798ff442e4b8109beaca854e40
runpixelrun <justindavidson23@gmail.com>
2016-04-26 03:48:34 +0000

    Post assessment 109 push

---
+++ Object_oriented_programming/oo_readings/exercises_1.rb
@@ -0,0 +1,57 @@
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+   
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+  
+  def speed_up(number)
+    @current_speed += number
+    puts "You hit the gas and sped up to #{@current_speed}kms/hr!"
+  end
+  
+  def brake(number)
+    if @current_speed - number <= 0
+        @current_speed = 0
+        puts "Your car has stopped!"
+    else
+        @current_speed -= number
+        puts "You hit the brake and your speed is now #{@current_speed}kms/hr"
+    end
+  end
+  
+  def shut_off
+    @current_speed = 0
+    puts "You have stopped the car"
+  end
+  
+  def current_speed
+    puts "You are now going #{@current_speed}kms/hr"
+  end
+  
+  def spray_paint(color)
+    self.color = color
+    puts "Nice!..Your car is now #{color}!"
+  end
+end
+
+first_car = MyCar.new(1997, 'green', 'lancer')
+first_car.speed_up(60)
+first_car.current_speed
+first_car.brake(80)
+first_car.current_speed
+first_car.speed_up(100)
+first_car.current_speed
+first_car.brake(80)
+first_car.current_speed
+first_car.shut_off
+first_car.current_speed
+puts first_car.color
+first_car.color = 'black'
+puts first_car.color
+puts first_car.year
+first_car.spray_paint('yellow')
